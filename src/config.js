212b6b3437b120fc635d180b733d736a10721b35
Suryabehara-1995 <surya.behara@a2znaturopathy.com>
2025-04-09 16:26:40 +0530

    Remove node_modules from repo and update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    //apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "https://tgh-pack-console.onrender.com", // Default to localhost
-    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000", // Default to localhost
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "https://tgh-pack-console.onrender.com", // Default to localhost
+    //apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000", // Default to localhost
 
   };
   
