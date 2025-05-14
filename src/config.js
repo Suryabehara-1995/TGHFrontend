fd284e9c756b249e7f5838e05669f7a5f81dc1e4
Suryabehara-1995 <surya.behara@a2znaturopathy.com>
2025-05-14 09:29:02 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "https://tgh-pack-console.onrender.com", // Default to localhost
-    //apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000", // Default to localhost
-
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "https://thegoodhealthapi.onrender.com", // Default to localhost
+   // apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000", // Default to localhost
+  // apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "https://tgh-pack-console.onrender.com",
   };
   
   export default config;
